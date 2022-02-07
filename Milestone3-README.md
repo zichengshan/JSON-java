39459a08617aa5d49b516a03255af6adb40e4c45
Zicheng.S <zichengshan@gmail.com>
2022-02-07 11:09:00 -0800

    Final version
    1. update milestone3.readme
    2. modify the parse() and toJSONObject() method
    3. delete the redundant code

--- Milestone3-README.md
+++ Milestone3-README.md
@@ -2,12 +2,13 @@
 
 Group member: Zicheng Shan, Chenxu Wang
 
-#### newParse() and toJSONObject_newParse() methods are added in src/main/java/org.json/XML.java
-1. JSONObject toJSONObject_newParse(Reader reader, Function<String,String> func) [line 1514]
-   - Add a prefix to all of its keys
-   - Main method:
-     - Rewrite newParse() [line 1304] based on parse() method. Compared with original parse(), replace string and tagName variable with func.apply(string) and func.apply(tagName)
-     - Call the newParse() in toJSONObject_newParse(), put reader and func as input
+#### Parse() and toJSONObject() methods are added in src/main/java/org.json/XML.java
+1. JSONObject toJSONObject(Reader reader, Function<String,String> func) [line 1281]
+  - Rewrite toJSONObject() method, inputs are a reader and a lambda function
+  - Purpose: Add a prefix to all of its keys
+  - Main method:
+    - Rewrite Parse() [line 1071]. Compared with original parse(), replace string and tagName variable with func.apply(string) and func.apply(tagName)
+    - Call the new Parse() method in toJSONObject(), put reader and func as input
 
 #### Unit Tests are added in src/test/java/org.json.junit/MileStone2Test.java
 * The unit tests are based on two small test files (https://www.ics.uci.edu/~lopes/teaching/swe262p/M2Test.java and https://docs.microsoft.com/en-us/previous-versions/windows/desktop/ms762271(v=vs.85)).
@@ -15,7 +16,6 @@ Group member: Zicheng Shan, Chenxu Wang
 1. For JSONObject toJSONObject(Reader reader, Function<String,String> func) we tested adding "SWE262_" as prefix.
     - Both of two test cases are passed
     
-
 #### Performance comparison
 * Based on the average running time between in-library and outside. The method in library performance better.
 * file size: 210.4MB
--- src/test/java/org/json/junit/MileStone3Test.java
+++ src/test/java/org/json/junit/MileStone3Test.java
@@ -3,14 +3,15 @@ package org.json.junit;
 import org.json.JSONObject;
 import org.json.XML;
 import org.junit.Test;
-
 import java.io.IOException;
 import java.io.Reader;
 import java.io.StringReader;
-
 import static org.junit.Assert.assertEquals;
 
 public class MileStone3Test {
+    /**
+     * Test1
+     */
     @Test
     public void handleTransformation(){
         String xmlStr ="<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n"+
@@ -28,7 +29,7 @@ public class MileStone3Test {
             reader.close();
             assertEquals("Correct result.","{\"SWE262_contact\":{\"SWE262_name\":\"Crista Lopes\",\"SWE262_nick\":\"Crista\",\"SWE262_address\":{\"SWE262_street\":\"Ave of Nowhere\",\"SWE262_zipcode\":92614}}}",
                     jo.toString());
-            System.out.println(jo.toString(4));
+//            System.out.println(jo.toString(4));
         }
         catch (IOException e){
             System.out.println("Caught a IO Exception ");
@@ -36,6 +37,9 @@ public class MileStone3Test {
         }
     }
 
+    /**
+     * Test 2
+     */
     @Test
     public void handleTransformation2(){
         String xmlStr ="<?xml version=\"1.0\"?>\n" +
@@ -76,58 +80,7 @@ public class MileStone3Test {
             reader.close();
             assertEquals("Correct result.","{\"SWE262_catalog\":{\"SWE262_book\":[{\"SWE262_author\":\"Gambardella, Matthew\",\"SWE262_title\":\"XML Developer's Guide\",\"SWE262_publish_date\":\"2000-10-01\",\"SWE262_genre\":\"Computer\",\"SWE262_description\":\"An in-depth look at creating applications\\n            with XML.\",\"SWE262_id\":\"bk101\",\"SWE262_price\":44.95},{\"SWE262_author\":\"Ralls, Kim\",\"SWE262_title\":\"Midnight Rain\",\"SWE262_publish_date\":\"2000-12-16\",\"SWE262_genre\":\"Fantasy\",\"SWE262_description\":\"A former architect battles corporate zombies,\\n            an evil sorceress, and her own childhood to become queen\\n            of the world.\",\"SWE262_id\":\"bk102\",\"SWE262_price\":5.95},{\"SWE262_author\":\"Corets, Eva\",\"SWE262_title\":\"Maeve Ascendant\",\"SWE262_publish_date\":\"2000-11-17\",\"SWE262_genre\":\"Fantasy\",\"SWE262_description\":\"After the collapse of a nanotechnology\\n            society in England, the young survivors lay the\\n            foundation for a new society.\",\"SWE262_id\":\"bk103\",\"SWE262_price\":5.95}]}}",
                     jo.toString());
-            System.out.println(jo.toString(4));
-        }
-        catch (IOException e){
-            System.out.println("Caught a IO Exception ");
-            e.printStackTrace();
-        }
-    }
-
-    /**
-     * handleTransformation3_newParse() is used to test newParse() method
-     */
-    @Test
-    public void handleTransformation3_newParse(){
-        String xmlStr ="<?xml version=\"1.0\"?>\n" +
-                "<catalog>\n" +
-                "    <book id=\"bk101\">\n" +
-                "        <author>Gambardella, Matthew</author>\n" +
-                "        <title>XML Developer's Guide</title>\n" +
-                "        <genre>Computer</genre>\n" +
-                "        <price>44.95</price>\n" +
-                "        <publish_date>2000-10-01</publish_date>\n" +
-                "        <description>An in-depth look at creating applications\n" +
-                "            with XML.</description>\n" +
-                "    </book>\n" +
-                "    <book id=\"bk102\">\n" +
-                "        <author>Ralls, Kim</author>\n" +
-                "        <title>Midnight Rain</title>\n" +
-                "        <genre>Fantasy</genre>\n" +
-                "        <price>5.95</price>\n" +
-                "        <publish_date>2000-12-16</publish_date>\n" +
-                "        <description>A former architect battles corporate zombies,\n" +
-                "            an evil sorceress, and her own childhood to become queen\n" +
-                "            of the world.</description>\n" +
-                "    </book>\n" +
-                "    <book id=\"bk103\">\n" +
-                "        <author>Corets, Eva</author>\n" +
-                "        <title>Maeve Ascendant</title>\n" +
-                "        <genre>Fantasy</genre>\n" +
-                "        <price>5.95</price>\n" +
-                "        <publish_date>2000-11-17</publish_date>\n" +
-                "        <description>After the collapse of a nanotechnology\n" +
-                "            society in England, the young survivors lay the\n" +
-                "            foundation for a new society.</description>\n" +
-                "    </book>\n" +
-                "</catalog>";
-        try {
-            Reader reader = new StringReader(xmlStr);
-            JSONObject jsonObject = XML.toJSONObject_newParse(reader, s -> "SWE262_" + s);
-            reader.close();
-            assertEquals("Correct result.","{\"SWE262_catalog\":{\"SWE262_book\":[{\"SWE262_author\":\"Gambardella, Matthew\",\"SWE262_title\":\"XML Developer's Guide\",\"SWE262_publish_date\":\"2000-10-01\",\"SWE262_genre\":\"Computer\",\"SWE262_description\":\"An in-depth look at creating applications\\n            with XML.\",\"SWE262_id\":\"bk101\",\"SWE262_price\":44.95},{\"SWE262_author\":\"Ralls, Kim\",\"SWE262_title\":\"Midnight Rain\",\"SWE262_publish_date\":\"2000-12-16\",\"SWE262_genre\":\"Fantasy\",\"SWE262_description\":\"A former architect battles corporate zombies,\\n            an evil sorceress, and her own childhood to become queen\\n            of the world.\",\"SWE262_id\":\"bk102\",\"SWE262_price\":5.95},{\"SWE262_author\":\"Corets, Eva\",\"SWE262_title\":\"Maeve Ascendant\",\"SWE262_publish_date\":\"2000-11-17\",\"SWE262_genre\":\"Fantasy\",\"SWE262_description\":\"After the collapse of a nanotechnology\\n            society in England, the young survivors lay the\\n            foundation for a new society.\",\"SWE262_id\":\"bk103\",\"SWE262_price\":5.95}]}}",
-                    jsonObject.toString());
-            System.out.println(jsonObject.toString(4));
+//            System.out.println(jo.toString(4));
         }
         catch (IOException e){
             System.out.println("Caught a IO Exception ");
