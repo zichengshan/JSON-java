a42c0cab95146ea5217ac9f2e59247070605fadd
chenxu-wang <chenxuw4@uci.edu>
2022-03-06 15:37:57 -0800

    add Test Cases

--- src/test/java/org/json/junit/MileStone5Test.java
+++ src/test/java/org/json/junit/MileStone5Test.java
@@ -6,26 +6,61 @@ import org.junit.Test;
 import java.io.Reader;
 import java.io.StringReader;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class MileStone5Test {
-    @Test
-    public void testAsyncToJson() throws ExecutionException, InterruptedException {
-//
-//        Reader reader = new StringReader();
-//        JSONObject objTest = XML.toJSONObjectMileS5test(reader, true);
-//        assertEquals(objTest.toString(), );
+    private String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" +
+            "<contact>\n" +
+            "  <nick>Crista </nick>\n" +
+            "  <name>Crista Lopes</name>\n" +
+            "  <address>\n" +
+            "    <street>Ave of Nowhere</street>\n" +
+            "    <zipcode>92614</zipcode>\n" +
+            "  </address>\n" +
+            "</contact>";
+    private String result = "{\"contact\":{\"nick\":\"Crista\",\"address\":{\"zipcode\":92614,\"street\":\"Ave of Nowhere\"},\"name\":\"Crista Lopes\"}}";
 
-//        Future<JSONObject> future = XML.toJSONObjectMileS5(reader, true);
-//        JSONObject obj = null;
-//        try{
-//            obj = future.get();
-//        }catch (InterruptedException e) {
-//            System.out.println("InterruptedException");
-//            e.printStackTrace();
-//        } catch (ExecutionException e) {
-//            System.out.println("ExecutionException");
-//            e.printStackTrace();
-//        }
+    /**
+     * Test whether the function can handle with XML successfully.
+     * @throws ExecutionException
+     * @throws InterruptedException
+     */
+    @Test
+    public void testSuccess() throws ExecutionException, InterruptedException {
+
+        Reader reader = new StringReader(xml);
+
+        Future<JSONObject> future = XML.toJSONObjectMS5(reader);
+        JSONObject obj = null;
+        try{
+            obj = future.get();
+            assertEquals(obj.toString(),result);
+        }catch (InterruptedException e) {
+            System.out.println("InterruptedException");
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            System.out.println("ExecutionException");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Test whether the function can handle with exception.
+     * @throws InterruptedException
+     * @throws ExecutionException
+     */
+    @Test(expected=ExecutionException.class)
+    public void shouldThrowExecutionException() throws InterruptedException, ExecutionException {
+        String str = "<contact";
+        Reader reader = new StringReader(str);
+        Future<JSONObject> futureJSONObject = XML.toJSONObjectMS5(reader);
+        while (!futureJSONObject.isDone()) {
+            Thread.sleep(1000);
+        }
+        JSONObject jo = futureJSONObject.get();
+        assertTrue("The Json would be empty because of exception", jo.isEmpty());
     }
 }
